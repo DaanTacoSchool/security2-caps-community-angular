cd1a15bc02160bdd245526219658f85b92436a35
DaanTacoSchool <djmessen@avans.nl>
2017-12-18 11:56:05 +0100

    Added initial posts and updated tslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PostComponent } from './post/post.component';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { PostItemComponent } from './post/post-list/post-item/post-item.component';
+import { PostDetailComponent } from './post/post-detail/post-detail.component';
+import { PostStartComponent } from './post/post-start/post-start.component';
+import {PostService} from "./post/post.service";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    PostListComponent,
+    PostItemComponent,
+    PostDetailComponent,
+    PostStartComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
