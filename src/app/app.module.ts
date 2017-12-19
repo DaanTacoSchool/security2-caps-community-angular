0906c51a03dba65ac813ba744deffbd786aeb273
DaanTacoSchool <djmessen@avans.nl>
2017-12-19 11:18:48 +0100

    added like and comment components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { PostListComponent } from './post/post-list/post-list.component';
 import { PostItemComponent } from './post/post-list/post-item/post-item.component';
 import { PostDetailComponent } from './post/post-detail/post-detail.component';
 import { PostStartComponent } from './post/post-start/post-start.component';
-import {PostService} from "./post/post.service";
+import { PostService } from "./post/post.service";
+import { CommentComponent } from './comment/comment.component';
+import { CommentListComponent } from './comment/comment-list/comment-list.component';
+import { CommentItemComponent } from './comment/comment-list/comment-item/comment-item.component';
+import { CommentService } from "./comment/comment.service";
+import { LikeComponent } from './like/like.component';
+import { LikeService } from "./like/like.service";
 
 
 @NgModule({
@@ -18,12 +23,16 @@ import {PostService} from "./post/post.service";
     PostListComponent,
     PostItemComponent,
     PostDetailComponent,
-    PostStartComponent
+    PostStartComponent,
+    CommentComponent,
+    CommentListComponent,
+    CommentItemComponent,
+    LikeComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [PostService],
+  providers: [PostService, CommentService, LikeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
